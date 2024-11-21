717537ee9549bfb9424d53499f56a3985a24fe52
unidevel <unidevel@hotmail.com>
2024-11-21 14:31:13 +0800

    adjust top menus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,22 +8,24 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
+const repoUrl = 'https://github.com/unidevel/wikisite';
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Presto & Gluten',
+  tagline: 'Tutorials and blogs',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://unidevel.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'unidevel', // Usually your GitHub org/user name.
+  projectName: 'Presto & Gluten wiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -46,7 +48,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            `${repoUrl}/blob/main`,
         },
         blog: {
           showReadingTime: true,
@@ -57,7 +59,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            `${repoUrl}/blob/main`,
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -76,21 +78,27 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        // title: 'Wiki Site',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Presto & Gluten Wiki Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'prestoSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Presto',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'glutenSidebar',
+            position: 'left',
+            label: 'Gluten',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: repoUrl,
             label: 'GitHub',
             position: 'right',
           },
@@ -103,8 +111,12 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Presto',
+                to: '/docs/presto/tutorial',
+              },
+              {
+                label: 'Gluten',
+                to: '/docs/gluten',
               },
             ],
           },
@@ -134,7 +146,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: repoUrl,
               },
             ],
           },
